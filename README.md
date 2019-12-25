c4303c5c548ca41c97e28911cc3ae98d248318c9
root <root@debian2>
2019-12-24 22:56:19 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#### Aplicación de comandos Tareas por hacer #########3
+
+Ejercicio de aprendiszaje de Node --- Tareas por Hacer ----
+
+
+Es necesario instalar los paquetes de Node con el comando 
+
+```
+npm install 
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar bien el archivo', err);
+    });
+}
+
+const cargardb = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+
+}
+
+const crear = (descripcion) => {
+
+    cargardb();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+const getListado = () => {
+    cargardb();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargardb();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargardb();
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
